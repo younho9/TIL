b15541163965aa86dc566d580bcaff63f0f3a907
Younho Choo <younho9.choo@gmail.com>
2020-06-12 11:50:33 +0900

    feat: Get contents from notion database (#20)

--- build.sh
+++ build.sh
@@ -3,32 +3,21 @@ pip install pipenv
 
 echo -------------------------------------------------------
 echo 🧱 Start build
-echo -------------------------------------------------------
-
 cd notion
 pipenv install
 
 echo -------------------------------------------------------
 echo 📝 Get TIL contents from notion
-echo -------------------------------------------------------
-
-pipenv run python main.py --directory=$1
+pipenv run python notion_to_til.py
 cd ..
 
 echo -------------------------------------------------------
 echo 💄 Apply prettier
-echo -------------------------------------------------------
-
 npx prettier --write docs/**/*.md
-
-echo -------------------------------------------------------
 echo ✅ Successfully formatted
-echo -------------------------------------------------------
-
 cd docs
 
 echo -------------------------------------------------------
 echo 🚗 Run docsify server
-echo -------------------------------------------------------
 
 docsify serve

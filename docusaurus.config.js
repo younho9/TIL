e74fc508bd6ab09425096c4b1c19c13a3cd5be33
Younho Choo <younho9.choo@gmail.com>
2021-07-06 18:19:34 +0900

    feat: add splash images

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -205,6 +205,76 @@ module.exports = {
             color: 'rgb(255, 255, 255)',
           },
           {
+            tagName: 'link',
+            href: 'img/splash-screens/iphone5_splash.png',
+            media:
+              '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/iphone6_splash.png',
+            media:
+              '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/iphoneplus_splash.png',
+            media:
+              '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/iphonex_splash.png',
+            media:
+              '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/iphonexr_splash.png',
+            media:
+              '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/iphonexsmax_splash.png',
+            media:
+              '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/ipad_splash.png',
+            media:
+              '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/ipadpro1_splash.png',
+            media:
+              '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/ipadpro3_splash.png',
+            media:
+              '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
+            tagName: 'link',
+            href: 'img/splash-screens/ipadpro2_splash.png',
+            media:
+              '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+          },
+          {
             tagName: 'meta',
             name: 'msapplication-TileImage',
             content: 'img/logo.png',
